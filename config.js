4ca5773cfe7e60c44852c338f2a5a8e50fef94d5
gorgovic <gorgovic>
2018-12-18 20:11:39 +0100

    Fix credentials

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
 	port: 3000,
 	mongodb: "mongodb://localhost/plantage",
 	credentials: {
-		privateKey: fs.readFileSync("/etc/letsencrypt/live/wasstahtufemplan.ga/privkey.pem", "utf8"),
-		certificate: fs.readFileSync("/etc/letsencrypt/live/wasstahtufemplan.ga/cert.pem", "utf8"),
-		ca: fs.readFileSync("/etc/letsencrypt/live/wasstahtufemplan.ga/chain.pem", "utf8")
+		key: fs.readFileSync("/etc/letsencrypt/live/wasstahtufemplan.ga/privkey.pem", "utf8"),
+		cert: fs.readFileSync("/etc/letsencrypt/live/wasstahtufemplan.ga/fullchain.pem", "utf8")
 	}
 };
